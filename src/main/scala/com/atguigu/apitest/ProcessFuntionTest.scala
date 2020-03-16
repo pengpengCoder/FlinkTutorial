e13ff7a82453403246f3d1b47beb3b1d30a6adf1
wushengran <shengran.wu@163.com>
2020-03-16 17:30:13 +0800

    Day 3 - window api & process function

---
+++ src/main/scala/com/atguigu/apitest/ProcessFuntionTest.scala
@@ -0,0 +1,74 @@
+package com.atguigu.apitest
+
+import org.apache.flink.api.common.state.{ValueState, ValueStateDescriptor}
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.util.Collector
+
+/**
+  * Copyright (c) 2018-2028 尚硅谷 All Rights Reserved 
+  *
+  * Project: FlinkTutorial
+  * Package: com.atguigu.apitest
+  * Version: 1.0
+  *
+  * Created by wushengran on 2020/3/16 16:59
+  */
+object ProcessFuntionTest {
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setParallelism(1)
+
+    val inputStream: DataStream[String] = env.socketTextStream("localhost", 7777)
+
+    // 基本转换操作，转换成样例类类型
+    val dataStream: DataStream[SensorReading] = inputStream
+      .map(data => {
+        val dataArray = data.split(",")
+        SensorReading(dataArray(0), dataArray(1).toLong, dataArray(2).toDouble)
+      })
+
+    val warningStream = dataStream
+      .keyBy(_.id)
+      .process( new TempIncreWarning(5000L) )
+
+    warningStream.print()
+    env.execute()
+  }
+}
+
+// 自定义 Process Function，用来定义定时器，检测温度连续上升
+class TempIncreWarning(interval: Long) extends KeyedProcessFunction[String, SensorReading, String]{
+  // 把上一次的温度值，保存成状态
+  lazy val lastTempState: ValueState[Double] = getRuntimeContext.getState(new ValueStateDescriptor[Double]("last-temp", classOf[Double]))
+  // 把注册的定时器时间戳保存成状态，方便删除
+  lazy val curTimerState: ValueState[Long] = getRuntimeContext.getState(new ValueStateDescriptor[Long]("cur-timer", classOf[Long]))
+
+  override def processElement(value: SensorReading, ctx: KeyedProcessFunction[String, SensorReading, String]#Context, out: Collector[String]): Unit = {
+    // 先把状态取出来
+    val lastTemp = lastTempState.value()
+    val curTimerTs = curTimerState.value()
+
+    lastTempState.update(value.temperature)
+
+    // 判断：如果温度上升并且没有定时器，那么注册定时器
+    if( value.temperature > lastTemp && curTimerTs == 0 ){
+      val ts = ctx.timerService().currentProcessingTime() + interval
+      ctx.timerService().registerProcessingTimeTimer(ts)
+      // 保存ts到状态
+      curTimerState.update(ts)
+    } else if( value.temperature < lastTemp ){
+      // 如果温度值下降，直接删除定时器
+      ctx.timerService().deleteProcessingTimeTimer(curTimerTs)
+      // 清空状态
+      curTimerState.clear()
+    }
+  }
+
+  override def onTimer(timestamp: Long, ctx: KeyedProcessFunction[String, SensorReading, String]#OnTimerContext, out: Collector[String]): Unit = {
+    // 定时器触发，报警
+    out.collect("传感器"+ ctx.getCurrentKey + "温度连续5秒上升")
+    // timer状态清空
+    curTimerState.clear()
+  }
+}
---
+++ src/main/scala/com/atguigu/apitest/WindowTest.scala
@@ -0,0 +1,115 @@
+package com.atguigu.apitest
+
+import org.apache.flink.api.common.functions.ReduceFunction
+import org.apache.flink.api.java.tuple.Tuple
+import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.functions.{AssignerWithPeriodicWatermarks, AssignerWithPunctuatedWatermarks}
+import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.scala.function.WindowFunction
+import org.apache.flink.streaming.api.watermark.Watermark
+import org.apache.flink.streaming.api.windowing.assigners.{EventTimeSessionWindows, SlidingProcessingTimeWindows}
+import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow
+import org.apache.flink.util.Collector
+
+/**
+  * Copyright (c) 2018-2028 尚硅谷 All Rights Reserved 
+  *
+  * Project: FlinkTutorial
+  * Package: com.atguigu.apitest
+  * Version: 1.0
+  *
+  * Created by wushengran on 2020/3/16 9:34
+  */
+object WindowTest {
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setParallelism(1)
+    env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
+    env.getConfig.setAutoWatermarkInterval(300L)
+
+    // 读取数据
+//    val inputStream: DataStream[String] = env.readTextFile("D:\\Projects\\BigData\\FlinkTutorial\\src\\main\\resources\\sensor.txt")
+    val inputStream: DataStream[String] = env.socketTextStream("localhost", 7777)
+
+    // 基本转换操作，转换成样例类类型
+    val dataStream: DataStream[SensorReading] = inputStream
+      .map(data => {
+        val dataArray = data.split(",")
+        SensorReading(dataArray(0), dataArray(1).toLong, dataArray(2).toDouble)
+      })
+//      .assignAscendingTimestamps( _.timestamp * 1000L )
+//      .assignTimestampsAndWatermarks(new MyPeriodicAssigner(1000L))
+      .assignTimestampsAndWatermarks( new BoundedOutOfOrdernessTimestampExtractor[SensorReading](Time.seconds(1)) {
+        override def extractTimestamp(element: SensorReading): Long = {
+          // 从数据中提取时间戳
+          element.timestamp * 1000L
+        }
+      } )
+
+//    dataStream.keyBy("id")
+//      .window( SlidingProcessingTimeWindows.of(Time.hours(1), Time.seconds(1), Time.hours(-8)) )
+//      .countWindow(10, 2)
+//      .timeWindow(Time.hours(1))
+//      .trigger()
+//      .evictor()
+//      .allowedLateness(Time.minutes(10))
+//      .sideOutputLateData()
+//      .sum(2)
+//      .getSideOutput()
+//      .apply( new MyWindowFunction() )
+
+    // 输出每15秒每个传感器温度的最小值
+    val minTempPerWinStream: DataStream[(String, Double)] = dataStream
+      .map( data => (data.id, data.temperature) )
+      .keyBy(_._1)    // 按照sensor id分组
+      .timeWindow(Time.seconds(15))    // 15秒的滚动窗口
+//      .reduce( (curMin, newData) => (curMin._1, curMin._2.min(newData._2)) )
+      .reduce( new MyReduceFunction() )
+
+    minTempPerWinStream.print()
+    env.execute("window test job")
+  }
+}
+
+// 自定义一个 window function
+class MyWindowFunction() extends WindowFunction[SensorReading, String, Tuple, TimeWindow]{
+  override def apply(key: Tuple, window: TimeWindow, input: Iterable[SensorReading], out: Collector[String]): Unit = {
+    out.collect(key + " " + window.getStart)
+  }
+}
+
+// 自定义一个增量聚合函数
+class MyReduceFunction() extends ReduceFunction[(String, Double)]{
+  override def reduce(value1: (String, Double), value2: (String, Double)): (String, Double) = {
+    ( value1._1, value1._2.min(value2._2) )
+  }
+}
+
+// 自定义一个周期性生成Watermark的assigner
+class MyPeriodicAssigner(bound: Long) extends AssignerWithPeriodicWatermarks[SensorReading]{
+  // 定义当前最大时间戳
+  var maxTs = Long.MinValue
+
+  override def getCurrentWatermark: Watermark = new Watermark(maxTs - bound)
+
+  override def extractTimestamp(element: SensorReading, previousElementTimestamp: Long): Long = {
+    maxTs = maxTs.max(element.timestamp * 1000L)
+    element.timestamp * 1000L
+  }
+}
+
+// 自定义可间断地生成watermark的 assiger
+class MyPuntuatedAssigner extends AssignerWithPunctuatedWatermarks[SensorReading]{
+  override def checkAndGetNextWatermark(lastElement: SensorReading, extractedTimestamp: Long): Watermark = {
+    if(lastElement.id == "sensor_1"){
+      new Watermark(extractedTimestamp)
+    } else{
+      null
+    }
+  }
+  override def extractTimestamp(element: SensorReading, previousElementTimestamp: Long): Long = {
+    element.timestamp * 1000L
+  }
+}
